6c296194eaf32eed496f02c6a7577f909529c97e
Nazarov Behruzbek <behruzbeknazarov001@gmail.com>
2024-05-24 14:09:12 +0500

    document samples

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from 'react';
+import Header from "@/srccomponents/Header/Header";
+import Link from 'next/link';
 
 const Resources = () => {
   return (
-    <div>Resources</div>
+    <>
+      <Header />
+      <div className="service-fon">
+        <div className="container">
+          <div className=" pt-[15%]">
+            <h2 className="text-white text-5xl font-500">Document Samples</h2>
+            <h3 className="text-white text-2xl mt-8">Home <span className="text-gray-400 text-2xl mt-9">- Document Samples</span></h3>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-[#F4F5F7] w-[100%] p-12">
+
+        <div className="container mx-auto grid grid-cols-[repeat(auto-fit,minmax(400px,1fr))] gap-8 ">
+         <div className="bg-white p-10 rounded-[5px]  flex ">
+           <div className='w-30 mr-12 flex items-center '>
+            <img className="w-[150px] h-[150px]" src="/document-icon.svg" alt="Error" />
+           </div>
+           <div className='w-70'>
+              <p className="text-custom-dark text-xl font-bold mb-2">Arizalar</p>
+              <Link href="#" className="text-blue-500 blok"> Бошқа фуқаролик низолари бўйича аризалар</Link>
+              <Link href="#" className="text-blue-500 blok"> Оилавий низолар бўйича аризалар</Link>
+              <Link href="#" className="text-blue-500 blok">  Уй-жой низолари бўйича аризалар</Link>
+            </div>
+           </div>
+
+           <div className="bg-white p-10 rounded-[5px]  flex ">
+           <div className='w-30 mr-12 flex items-center '>
+            <img className="w-[150px] h-[150px]" src="/document-icon.svg" alt="Error" />
+           </div>
+           <div className='w-70'>
+              <p className="text-custom-dark text-xl font-bold mb-2">Shartnomalar</p>
+              <Link href="#" className="text-blue-500 blok"> Бошқа фуқаролик низолари бўйича аризалар</Link>
+              <Link href="#" className="text-blue-500 blok"> Оилавий низолар бўйича аризалар</Link>
+              <Link href="#" className="text-blue-500 blok">  Уй-жой низолари бўйича аризалар</Link>
+            </div>
+           </div>
+
+           <div className="bg-white p-10 rounded-[5px]  flex ">
+           <div className='w-30 mr-12 flex items-center '>
+            <img className="w-[150px] h-[150px]" src="/document-icon.svg" alt="Error" />
+           </div>
+           <div className='w-70'>
+              <p className="text-custom-dark text-xl font-bold mb-2">Talabnoma</p>
+              <Link href="#" className="text-blue-500 blok"> Бошқа фуқаролик низолари бўйича аризалар</Link>
+              <Link href="#" className="text-blue-500 blok"> Оилавий низолар бўйича аризалар</Link>
+              <Link href="#" className="text-blue-500 blok">  Уй-жой низолари бўйича аризалар</Link>
+            </div>
+           </div>
+        </div>
+        
+      </div>
+    </>
   )
 }
 
-export default Resources
+export default Resources;
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "@/srccomponents/Header/Header";
-import './services.css'
+
 
 const Services = () => {
   return (
